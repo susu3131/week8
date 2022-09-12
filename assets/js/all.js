7b23cfeeefaf610dba4d019d3ab9baba7a4c08f1
您的姓名 <anny81327@gmail.com>
2022-09-12 16:31:56 +0800

    Update 2022-09-12T08:31:50.972Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,14 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+$(document).ready(function () {
+  $(".close").click(function (e) {
+    e.preventDefault();
+    $(".search").toggle();
+  });
+  $(".close").click(function (e) {
+    e.preventDefault();
+    $(".navbar").toggle();
+  });
+});
 //# sourceMappingURL=all.js.map
