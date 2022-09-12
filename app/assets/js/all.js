bcc11b11bb6e6333d5386143192c58dccf58ed0e
您的姓名 <anny81327@gmail.com>
2022-09-12 16:32:26 +0800

    nav完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,20 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+$(document).ready(function () {
+  
+ $(".close").click(function (e) { 
+  e.preventDefault();
+  $(".search").toggle();
+
+ });
+
+   
+ $(".close").click(function (e) { 
+  e.preventDefault();
+  $(".navbar").toggle();
+
+ });
+
+});
