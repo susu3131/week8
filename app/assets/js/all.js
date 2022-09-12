0c84fd6e89fe1d49f02f5005f0b947469dc284ab
susu3131 <anny81327@gmail.com>
2022-09-13 01:22:28 +0800

    swiper 80%

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,20 +1,32 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
 });
 
 $(document).ready(function () {
-  
- $(".close").click(function (e) { 
-  e.preventDefault();
-  $(".search").toggle();
 
- });
+  $(".close").click(function (e) {
+    e.preventDefault();
+    $(".search").toggle();
 
-   
- $(".close").click(function (e) { 
-  e.preventDefault();
-  $(".navbar").toggle();
+  });
 
- });
 
+  $(".close").click(function (e) {
+    e.preventDefault();
+    $(".navbar").toggle();
+
+  });
+
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2.4,
+  spaceBetween: 80,
+  centeredSlides:true,
+  loop:true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
