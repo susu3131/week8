f20bbf86f0d21529341350346cd07260b5721131
susu3131 <anny81327@gmail.com>
2022-09-13 12:20:35 +0800

    swiper 100%

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -21,12 +21,32 @@ $(document).ready(function () {
 
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2.4,
-  spaceBetween: 80,
+  slidesPerView: 1,
+  spaceBetween: 0,
   centeredSlides:true,
   loop:true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 1.3,
+      spaceBetween: 50,
+    },
+    768: {
+      slidesPerView: 1.5,
+      spaceBetween: 100,
+    },
+    992: {
+      slidesPerView: 1.8,
+      spaceBetween: 80,
+    },
+    1200: {
+      slidesPerView: 2.4,
+      spaceBetween: 80,
+    },
+  }
 });
